e76606aeeb70dc0919e0e0aea8317aa72bff7ecb
Sora Mas <68703088+Bumvolla@users.noreply.github.com>
2026-02-04 15:46:18 +0100

    swipe deetction rewrite

--- Assets/Scripts/SwipeDetection.cs
+++ Assets/Scripts/SwipeDetection.cs
@@ -1,18 +1,28 @@
-using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class SwipeDetection : MonoBehaviour
 {
+    [SerializeField] TextMeshPro SwipeResistence;
+    [SerializeField] TextMeshPro SwipeStart;
+    [SerializeField] TextMeshPro SwipeEnd;
+    [SerializeField] TextMeshPro SwipeDelta;
+
     public static SwipeDetection instance;
     public UnityEvent<Vector2> swipePerformed;
     public UnityEvent pressPerformed;
-    [SerializeField] private InputAction position, press, move, parry;
 
-    [SerializeField] private float swipeResistance = 100;
+    [SerializeField] private InputAction position, press, move, parry;
+    [SerializeField] private float swipeResistance = 100f;
+
     private Vector2 startPos;
-    private Vector2 currentPos => position.ReadValue<Vector2>();
+    private Vector2 lastPos;
+
+    private bool swipeCommitted;
+
+    private Vector2 currentPos => lastPos;
 
     private void Awake()
     {
@@ -20,31 +30,79 @@ public class SwipeDetection : MonoBehaviour
         press.Enable();
         move.Enable();
         parry.Enable();
-        press.performed += _ => { startPos = currentPos; Debug.Log("Press performed at pos " + currentPos); pressPerformed.Invoke(); };
-        press.canceled += _ => { DetectSwipe(); Debug.Log("Press canceled at pos " + currentPos); };
-        move.performed += _ => { swipePerformed.Invoke(new Vector2(move.ReadValue<float>(), 0)); };
-        parry.performed += _ => { swipePerformed.Invoke(new Vector2(0, 1)); };
+
+        position.performed += ctx =>
+        {
+            lastPos = ctx.ReadValue<Vector2>();
+            DetectSwipe(); // detect during movement
+        };
+
+        press.performed += _ =>
+        {
+            startPos = currentPos;
+            swipeCommitted = false;
+
+            SwipeStart.text = "Swipe started at: " + startPos;
+            pressPerformed.Invoke();
+        };
+
+        press.canceled += _ =>
+        {
+            SwipeEnd.text = "Swipe ended at: " + currentPos;
+        };
+
+        move.performed += _ =>
+        {
+            swipePerformed.Invoke(new Vector2(move.ReadValue<float>(), 0));
+        };
+
+        parry.performed += _ =>
+        {
+            swipePerformed.Invoke(new Vector2(0, 1));
+        };
+
         instance = this;
+    }
+
+    private void Start()
+    {
+        SwipeResistence.text = "Current swipe resistence: " + swipeResistance;
     }
 
     private void DetectSwipe()
     {
+        if (swipeCommitted)
+            return;
+
         Vector2 delta = currentPos - startPos;
-        Vector2 direction = Vector2.zero;
 
-        if (Mathf.Abs(delta.x) > swipeResistance)
+        SwipeDelta.text =
+            "Last delta: " +
+            Mathf.Abs(delta.x).ToString("F1") + ", " +
+            Mathf.Abs(delta.y).ToString("F1");
+
+        // Dead zone
+        if (delta.magnitude < swipeResistance)
+            return;
+
+        Vector2 direction;
+
+        // Lock to dominant axis
+        if (Mathf.Abs(delta.x) >= Mathf.Abs(delta.y))
         {
-            direction.x = Mathf.Clamp(delta.x, -1, 1);
+            direction = new Vector2(Mathf.Sign(delta.x), 0);
         }
-        if (Mathf.Abs(delta.y) > swipeResistance)
+        else
         {
-            direction.y = Mathf.Clamp(delta.y, -1, 1);
-        }
-        if (direction != Vector2.zero)
-        {
-            swipePerformed.Invoke(direction);
-            Debug.Log("Swipe detexted, direction:" + (direction.x > 0 ? "x" : "-x"));
+            direction = new Vector2(0, Mathf.Sign(delta.y));
         }
 
+        swipeCommitted = true;
+        swipePerformed.Invoke(direction);
+
+        Debug.Log(
+            "Swipe detected: " +
+            (direction.x != 0 ? (direction.x > 0 ? "Right" : "Left") : "Up")
+        );
     }
 }
